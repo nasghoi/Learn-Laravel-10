097581a924cf9165882d164b8e2b3245cf36645a
Nasrullah Jam Sarim <muhdnas10@gmail.com>
2024-02-19 15:26:39 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -40,6 +43,20 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        'password' => 'hashed', // here bcrypt function
     ];
+
+    protected function name(): Attribute 
+    {
+        return Attribute::make(
+            get: fn($value) => Str::upper($value)
+        );
+    }
+
+    // protected function password(): Attribute 
+    // {
+    //     return Attribute::make(
+    //         set: fn($value) => bcrypt($value)
+    //     );
+    // }
 }
